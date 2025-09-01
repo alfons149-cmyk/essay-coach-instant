e8b278b8bcbf74ea85264904076ad3c8f1d8aabc
alfons149-cmyk <alfons149@gmail.com>
2025-09-01 16:55:03 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 */
 
 const REPO_BASE = '/essay-coach-instant'; // ✅ update
-const CACHE_VERSION = 'v¡53';              // 🔼 bump this
+const CACHE_VERSION = 'v¡54';              // 🔼 bump this
 
 const STATIC_CACHE  = `essaycoach-static-${CACHE_VERSION}`;
 const RUNTIME_CACHE = `essaycoach-runtime-${CACHE_VERSION}`;
