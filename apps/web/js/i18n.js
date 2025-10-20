b9fe8210c365fd72c2ea23976be74d66eb2ce5f4
alfons149-cmyk <alfons149@gmail.com>
2025-10-20 23:01:29 +0200

    Update i18n.js

--- apps/web/js/i18n.js
+++ apps/web/js/i18n.js
@@ -1,1 +1,11 @@
-
+async function loadI18n(lang){
+const res = await fetch(`./assets/i18n/${lang}.json`);
+if(!res.ok) throw new Error('i18n load failed');
+return res.json();
+}
+function applyI18n(dict){
+document.querySelectorAll('[data-i18n]').forEach(el=>{
+const key=el.getAttribute('data-i18n');
+if(dict[key]) el.textContent = dict[key];
+});
+}
